0dafe1aed00ee2864b1a395ce820e35d831e92a4
Joel Lewenstein <joel.lewenstein@gmail.com>
2019-01-12 15:55:49 -0800

    v2

---
+++ v1/index_files/main.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+
+  $(".image").fancybox({
+        padding: 0,
+
+        //autoCenter: true,
+
+        openEffect : 'elastic',
+        openSpeed  : 75,
+
+        closeEffect : 'elastic',
+        closeSpeed  : 75,
+
+        closeClick : true,
+
+        // helpers : {
+        //   overlay : null
+        // }
+      });
+});
